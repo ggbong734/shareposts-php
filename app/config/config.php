1436fdff045d90adfaa57641cdba600d62f84c5c
Gerry Bong <ggbong734@gmail.com>
2022-11-28 22:24:26 -0800

    clean template

---
+++ app/config/config.php
@@ -0,0 +1,19 @@
+<?php
+
+  // DB Params
+  define('DB_HOST', 'localhost');
+  define('DB_USER', '_YOUR_USER');
+  define('DB_PASS', '_YOUR_PASS_');
+  define('DB_NAME', '_YOUR_DBNAME_');
+
+  // App Root
+  define('APPROOT', dirname(dirname(__FILE__)));
+
+  // URL Root
+  define('URLROOT', '_YOUR_URL_');
+
+  // Site name
+  define('SITENAME', '_YOUR_SITENAME_');
+
+  // Remember to change the .htaccess file under /public
+  // Rewrite Base __YOUR_ROOT_URL__
---
+++ app/views/pages/index.php
@@ -0,0 +1,7 @@
+<?php require APPROOT . '/views/inc/header.php'; ?>
+
+<h1><?php echo $data['title']?></h1>
+
+<p>This is the Traversy MVC PHP framework. Please refer to the docs and the config.php file on how to use it </p>
+
+<?php require APPROOT . '/views/inc/footer.php'; ?>
