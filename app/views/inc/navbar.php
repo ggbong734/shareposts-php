d0a66f9a2aebc0ed01bd35f8b9f94d5f8bf82da8
Gerry Bong <ggbong734@gmail.com>
2022-12-04 16:46:43 -0800

    added register and login pages

--- app/views/inc/navbar.php
+++ app/views/inc/navbar.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-3" aria-label="Fifth navbar example">
-    <div class="container-fluid">
+  <div class="container">
       <a class="navbar-brand" href="<?php echo URLROOT; ?>"><?php echo SITENAME; ?></a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample05" aria-controls="navbarsExample05" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -8,7 +8,7 @@
       <div class="collapse navbar-collapse" id="navbarsExample05">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="<?php echo URLROOT; ?>">Home</a>
+            <a class="nav-link" aria-current="page" href="<?php echo URLROOT; ?>">Home</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="<?php echo URLROOT; ?>/pages/about">About</a>
@@ -16,13 +16,13 @@
         </ul>
         <ul class="navbar-nav ml-auto">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="<?php echo URLROOT; ?>/users/register">Register</a>
+            <a class="nav-link" aria-current="page" href="<?php echo URLROOT; ?>/users/register">Register</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="<?php echo URLROOT; ?>/users/login">Login</a>
           </li>
         </ul>
 
-      </div>
     </div>
+  </div>
   </nav>
