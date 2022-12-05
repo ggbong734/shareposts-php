e130f931916af2989485b7607c671b700698b73c
Gerry Bong <ggbong734@gmail.com>
2022-12-04 17:22:35 -0800

    added validation for register and login pages

--- app/views/users/register.php
+++ app/views/users/register.php
@@ -9,25 +9,25 @@
           <label for="name">Name: <sup>*</sup></label>
           <input type="text" name="name" class="form-control form-control-lg <?php echo (!empty($data['name_err'])) ? 'is-invalid' : ''; ?>"
           value="<?php echo $data['name']; ?>">
-          <span class="invalid-feedback"><?echo $data['name_err'];?></span>
+          <span class="invalid-feedback"><?php echo $data['name_err'];?></span>
         </div>
         <div class="form-group">
           <label for="email">Email: <sup>*</sup></label>
           <input type="email" name="email" class="form-control form-control-lg <?php echo (!empty($data['email_err'])) ? 'is-invalid' : ''; ?>"
           value="<?php echo $data['email']; ?>">
-          <span class="invalid-feedback"><?echo $data['email_err'];?></span>
+          <span class="invalid-feedback"><?php echo $data['email_err'];?></span>
         </div>
         <div class="form-group">
           <label for="password">Password: <sup>*</sup></label>
           <input type="password" name="password" class="form-control form-control-lg <?php echo (!empty($data['password_err'])) ? 'is-invalid' : ''; ?>"
           value="<?php echo $data['password']; ?>">
-          <span class="invalid-feedback"><?echo $data['password_err'];?></span>
+          <span class="invalid-feedback"><?php echo $data['password_err'];?></span>
         </div>
         <div class="form-group">
           <label for="confirm_password">Confirm Password: <sup>*</sup></label>
           <input type="password" name="confirm_password" class="form-control form-control-lg <?php echo (!empty($data['confirm_password_err'])) ? 'is-invalid' : ''; ?>"
           value="<?php echo $data['confirm_password']; ?>">
-          <span class="invalid-feedback"><?echo $data['confirm_password_err'];?></span>
+          <span class="invalid-feedback"><?php echo $data['confirm_password_err'];?></span>
         </div>
         <div class="row mt-2">
           <div class="col">
