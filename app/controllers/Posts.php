cae71b0ce9991215c88babe2bbca530763d1ed38
Gerry Bong <ggbong734@gmail.com>
2022-12-12 22:14:36 -0800

    updated Posts controller

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -1,6 +1,13 @@
 <?php
 
 class Posts extends Controller {
+  public function __construct(){
+    // if not logged in
+    if(!isLoggedIn()){
+      redirect('users/login');
+    }
+  }
+
   public function index(){
     $data = [];
     $this->view('posts/index', $data);
