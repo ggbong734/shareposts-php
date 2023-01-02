f1c933fdadc40b74a8d974777988e5f3050b7a26
Gerry Bong <ggbong734@gmail.com>
2023-01-01 23:19:39 -0800

    added edit post functionality

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -76,6 +76,7 @@ class Posts extends Controller {
       $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
 
       $data = [
+        'id' => $id,
         'title' => trim($_POST['title']),
         'body' => trim($_POST['body']),
         'user_id' => $_SESSION['user_id'],
@@ -95,15 +96,15 @@ class Posts extends Controller {
       // Make sure no errors
       if(empty($data['title_err']) && empty($data['body_err'])){
         // if no errors
-        if($this->postModel->addPost($data)) {
-          flash('post_message', 'Post Added');
+        if($this->postModel->updatePost($data)) {
+          flash('post_message', 'Post Updated');
           redirect('posts');
         } else {
           die('Something went wrong');
         }
       } else {
         // Load view with errors
-        $this->view('posts/add', $data);
+        $this->view('posts/edit', $data);
       }
 
     } else {
